39c68fb21bad49f0f31e6eba0e86e7f530e6a0dd
Deivi Patzi <patzivargasdavid@gmail.com>
2022-11-11 16:50:48 -0400

    C18 subiendo CAMBIOS heroku 11/11/2022

--- App.py
+++ App.py
@@ -1,21 +1,7 @@
-from flask import Flask,request
+from flask import Flask
 from rutas.login import login
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-#app.register_blueprint(login)
-CORS(app)
-# @app.route('/login',methods=['POST'])
-# def ingreso_login():
-#     if request.method == 'POST':
-#         usuario = request.form['usuario']
-#         password = request.form['password']
-#         print(usuario)
-#         print(password)
-#     return ('INSERTO EXITOSO')
-@app.route("/login", methods=['POST'])
-def add_user():
-    if request.method == 'POST':
-        print(request.form['usuario'])
-        print(request.form['password'])
-        return {"carnet":request.form('ci')}
+CORS(app,supports_credentials=True)
+app.register_blueprint(login)
--- rutas/login.py
+++ rutas/login.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
 from flask import request
-from flask_cors import CORS
+from flask_cors import cross_origin
 
 # con()
 login = Blueprint('login',__name__)
-CORS(login)
 @login.route('/',methods=['POST'])
+@cross_origin(supports_credentials=True)
 def ingreso_login():
     if request.method == 'POST':
         usuario = request.form['usuario']
