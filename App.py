e66b0c9c6891a8994746106ef2624ad0f8113c99
Deivi Patzi <patzivargasdavid@gmail.com>
2022-11-11 16:41:24 -0400

    C17 subiendo CAMBIOS heroku 11/11/2022

--- App.py
+++ App.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask,request
 from rutas.login import login
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.register_blueprint(login)
+#app.register_blueprint(login)
+@app.route('/login',methods=['POST'])
+def ingreso_login():
+    if request.method == 'POST':
+        usuario = request.form['usuario']
+        password = request.form['password']
+        print(usuario)
+        print(password)
+    return ('INSERTO EXITOSO')
 CORS(app)
